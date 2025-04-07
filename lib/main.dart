0ee9b55650aa5d416603bafd6094b2ea700c6940
Alina-02 <alinamontma@gmail.com>
2025-04-07 22:04:01 +0200

    first approaches to flutter

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial1/pages/counter_page.dart';
+import 'package:tutorial1/pages/first_page.dart';
+import 'package:tutorial1/pages/home_page.dart';
+import 'package:tutorial1/pages/profile_page.dart';
+import 'package:tutorial1/pages/settings_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  /*
+    List<int> numbers = [1,2,3];
+
+    Set<String> uniqueNames = {"Mitch", "Sharon", "Vince"};
+
+    Map user = {
+      "name": "Mitch",
+      "age": 26,
+      "height": 180
+    };
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterPage(),
+      routes: {
+        '/firstpage': (context) => FirstPage(),
+        '/homepage': (context) => HomePage(),
+        '/settingspage': (context) => SettingsPage(),
+        '/profilepage': (context) => ProfilePage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  // variable
+  int _counter = 0;
+
+  // method
+  void _incrementCounter() {
+    _counter++;
+  }
+
+  // UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
